f324891ef30e6c84991b493616afe1e1dc2f7193
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-12-28 15:38:46 +0700

    refactor: 💡 change routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,3 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [
-  index("routes/home.tsx"),
-  route("track-one", "routes/track-one/index.tsx"),
-  route("track-two", "routes/track-two/index.tsx"),
-  route("track-three", "routes/track-three/index.tsx")
-] satisfies RouteConfig;
+export default [index("routes/index.tsx")] satisfies RouteConfig;
